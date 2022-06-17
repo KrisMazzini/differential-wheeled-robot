f69459d9de4f358a29f778842d4c411ca56cc734
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-06-17 18:55:05 -0300

    first commit

---
+++ differentialModel.m
@@ -0,0 +1,15 @@
+clear; close all; clc;
+
+robot = Robot;
+robot = robot.addPositionHistory;
+
+simTimeSampling = 0.25;
+simMaxTime = 30;
+
+for time = 0:simTimeSampling:simMaxTime
+  
+    robot = robot.move(simTimeSampling);
+    robot = robot.addPositionHistory;
+    robot.plotRobot;
+
+end
