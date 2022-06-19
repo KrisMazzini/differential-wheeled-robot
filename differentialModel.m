77398cd281787a1e8df5472c48e60a5cd9aee1aa
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-06-19 13:25:09 -0300

    minor plot adjustments

--- differentialModel.m
+++ differentialModel.m
@@ -1,12 +1,16 @@
 clear; close all; clc;
 
 initialPosition = [0; 0; 0];
-goalPosition = [10; 8; deg2rad(77)];
+goalPosition = [0; 10; deg2rad(-90)];
 
 robot = Robot(initialPosition);
 goal = Robot(goalPosition);
 
+fig = figure;
+fig.Position = [0, 0, 2000, 2000];
+
 plotRobot(robot, goal);
+pause;
 
 simTimeSampling = 0.2;
 
