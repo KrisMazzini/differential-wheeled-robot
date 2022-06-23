66f36e51ea90d7c953cb34a22ebb3127d7b3549d
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-06-23 20:32:56 -0300

    include collision zone

--- RobotBody.m
+++ RobotBody.m
@@ -3,6 +3,7 @@ classdef RobotBody
         center
         leftWheel
         rightWheel
+        collisionZone
     end
     properties
         position
@@ -43,6 +44,14 @@ classdef RobotBody
 
             robotRightWheel = obj.rotateZ(robotRightWheel);
             robotRightWheel = obj.translate(robotRightWheel);
+        end
+
+        function robotCollisionZone = get.collisionZone(obj)
+            fullBody = [obj.leftWheel obj.rightWheel obj.center];
+            robotCollisionZone = max(sqrt( ...
+                (fullBody(1,:) - obj.position(1)).^2 + ...
+                (fullBody(2,:) - obj.position(2)).^2 ...
+            ));
         end
 
         function outputMatrix = translate(obj, inputMatrix)
